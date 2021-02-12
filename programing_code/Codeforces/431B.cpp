3165e4030fbbce1e3bb1a636b700d65e96d235bd
Rakibul Islam <72851692+Rakibul73@users.noreply.github.com>
2021-02-12 16:05:39 +0600

    update

---
+++ programing_code/Codeforces/431B.cpp
@@ -0,0 +1,21 @@
+
+/***
+    Bismillahir Rahmanir Rahim
+    Read in the name of Allah, who created you.
+    Author : Md. Rakibul Islam,
+    Faculty of CSE, Patuakhali Science & Technology University, Bangladesh.
+***/
+
+// last modified: 2021/02/12 16:05:12
+
+
+
+#include <bits/stdc++.h> 
+using namespace std;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+int main() {
+    // Below function is A fast IO program
+    fast_cin();
+    
+    return 0;
+}
