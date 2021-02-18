6d4941d1939ff6042994fe46d075475631260526
Rakibul Islam <72851692+Rakibul73@users.noreply.github.com>
2021-02-18 19:59:24 +0600

    update

--- programing_code/Codeforces/test.cpp
+++ programing_code/Codeforces/test.cpp
@@ -6,43 +6,28 @@
     Faculty of CSE, Patuakhali Science & Technology University, Bangladesh.
 ***/
 
-// last modified: 2021/02/13 23:13:38
+// last modified: 2021/02/18 19:58:16
 
 
 
-#include <bits/stdc++.h>
-#include <map>
+#include <bits/stdc++.h> 
 using namespace std;
 #define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-std::map<char, int>
-storeStringFrequency(string str)
-{
-    map<char, int> M;
-    for (int i = 0; str[i]; i++) 
+
+vector<pair<int,int>>  cinvecpair(int n) {
+    int q, w;
+    vector<pair<int,int>> a;
+    for (int i = 0; i < n; i++)
     {
-        if (M.find(str[i]) == M.end()) 
-        {
-            M.insert(make_pair(str[i], 1));
-        }
-        else
-        {
-            M[str[i]]++;
-        }
+        cin>>q>>w;
+        a.push_back(make_pair(q,w));
+        
     }
-    return M;
+    return a;
 }
 int main() {
     // Below function is A fast IO program
     fast_cin();
-    string s1, s2;
-    getline(cin,s1);
-    getline(cin,s2);
-    map<char, int>x1, x2;
-    x1 = storeStringFrequency(s1);
-    x2 = storeStringFrequency(s2);
-    for(const auto & i : x2) {
-        
-    }
-
+    
     return 0;
 }
