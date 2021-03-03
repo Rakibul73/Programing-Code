800c055a7b0fc758ff0abb35e8fa7953ab2f4765
Rakibul Islam <72851692+Rakibul73@users.noreply.github.com>
2021-03-03 14:02:51 +0600

    update final

--- programing_code/Codeforces/1494A.cpp
+++ programing_code/Codeforces/1494A.cpp
@@ -6,7 +6,7 @@
     Faculty of CSE, Patuakhali Science & Technology University, Bangladesh.
 ***/
 
-// last modified: 2021/03/03 00:49:41
+// last modified: 2021/03/03 13:48:54
 
 
 
@@ -23,56 +23,58 @@ int main() {
     {
         string a;
         cin>>a;
-        char ff = a[0] , l = 'F', ll = a[a.size()-1], f = 'U';
-        int first = 0, last = 0;
+        char ff = a[0] , ll = a[a.size()-1];
         bool yoo = true;
         int i;
         if(ff == ll) {
             yoo = false;
         }
         else {
-            for (i = 1; i < a.size(); i++)
-            {
-                if(ff == a[i]) {
-                    continue;
-                }
-                if(ll == a[i]) {
-                    continue;
-                }
-                else if( ff != a[i] && ll != a[i]) {
-                    f = a[i];
-                    break;
-                }
-                else if( ff != a[i] && ll == a[i]) {
-                    continue;
-                }
-                else if( ff != a[i] && l != a[i]) {
-                    f = a[i];
-                    break;
-                }
-            }
             vector<int>g;
             for (int i = 0; i < a.size(); i++)
             {
                 if(ff == a[i]) {
                     g.push_back(1);
                 }
-                if(f == a[i]) {
+                else if(ll == a[i]) {
+                    g.push_back(-1);
+                }
+                else {
                     g.push_back(1);
-                }
-                if(ll == a[i]) {
-                    g.push_back(-1);
-                }
-                if(l == a[i]) {
-                    g.push_back(-1);
                 }
             }
             int sum = 0;
             for (int i = 0; i < g.size(); i++)
             {
                 sum = sum + g[i];
+                if(sum < 0) {
+                    break;
+                }
             }
-            if(sum == 0) {
+            //=================================================
+            g.clear();
+            for (int i = 0; i < a.size(); i++)
+            {
+                if(ff == a[i]) {
+                    g.push_back(1);
+                }
+                else if(ll == a[i]) {
+                    g.push_back(-1);
+                }
+                else {
+                    g.push_back(-1);
+                }
+            }
+            int sum2 = 0;
+            for (int i = 0; i < g.size(); i++)
+            {
+                sum2 = sum2 + g[i];
+                if(sum2 < 0) {
+                    break;
+                }
+            }
+
+            if(sum == 0 || sum2 == 0) {
                 yoo = true;
             }
             else {
