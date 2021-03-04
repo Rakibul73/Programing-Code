88df2fcc5a87be8913b69a372bd6ae7e5aaf94b0
Rakibul Islam <72851692+Rakibul73@users.noreply.github.com>
2021-03-04 13:30:02 +0600

    update

--- programing_code/Codeforces/1492B.cpp
+++ programing_code/Codeforces/1492B.cpp
@@ -6,47 +6,42 @@
     Faculty of CSE, Patuakhali Science & Technology University, Bangladesh.
 ***/
 
-// last modified: 2021/02/24 12:52:44
+// last modified: 2021/03/03 22:39:48
 
-
-
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
 using namespace std;
-#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define fast_cin()                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
 int main() {
     // Below function is A fast IO program
     fast_cin();
     int t;
-    cin>>t;
-    while (t)
-    {
-        int n, big = -1,index, index1;
-        cin>>n;
+    cin >> t;
+    while (t) {
+        int n, big = -1, index, index1;
+        cin >> n;
         vector<int> p(n), q;
-        for (int i = 0; i < n; i++)
-        {
-            cin>>p[i];
-            if(big < p[i]) {
+        for (int i = 0; i < n; i++) {
+            cin >> p[i];
+            if (big < p[i]) {
                 big = p[i];
                 q.push_back(i);
             }
         }
         reverse(q.begin(), q.end());
-        int r=n;
-        for(auto x: q){
-            for(int i = x; i < r; i++){
-                cout<<p[i]<<" ";
+        int r = n;
+        for (auto x : q) {
+            for (int i = x; i < r; i++) {
+                cout << p[i] << " ";
             }
-            r=x;
+            r = x;
         }
 
-
-
-
-        cout<<"\n";
+        cout << "\n";
         t--;
     }
-    
-    
+
     return 0;
 }
