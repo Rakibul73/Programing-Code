8afa169f42364bc1d16cfee5b594067eabd48a25
Rakibul Islam <72851692+Rakibul73@users.noreply.github.com>
2021-04-10 17:27:57 +0600

    first c# test

---
+++ programing_code/C#/Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string hh = Console.ReadLine();
+            ulong  gg = Convert.ToUInt64(hh);
+            Console.WriteLine(hh);
+            Console.WriteLine(gg);
+
+        }
+        
+    }
+}
