9444fba7b6a5e7dd8f1d5ab3b44fcc83bf721c51
Rakibul Islam <72851692+Rakibul73@users.noreply.github.com>
2021-05-22 19:38:38 +0600

    update

---
+++ programing_code/Codeforces/1527A.cpp
@@ -0,0 +1,45 @@
+
+/***
+    Bismillahir Rahmanir Rahim
+    Read in the name of Allah, who created you.
+    Author : Md. Rakibul Islam,
+    Faculty of CSE, Patuakhali Science & Technology University, Bangladesh.
+***/
+
+// last modified: 2021/05/22 19:32:19
+
+
+
+#include <bits/stdc++.h> 
+using namespace std;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+void solve() {
+    long long n, i = 1;
+    cin >> n;
+    
+    while (1)
+    {
+        if(n < i*2) {
+            cout << i - 1 << "\n";
+            break;
+        }
+        else {
+            i = i * 2;
+        }
+    }
+    return;
+}
+
+int main() {
+    // Below function is A fast IO program
+    fast_cin();
+    int t;
+    cin >> t;
+    //t = 1;
+    for (int i = 1; i <= t; i++) {
+        solve();
+    }
+    
+    return 0;
+}
