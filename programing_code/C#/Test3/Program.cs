adcb2c40a9bdd8f001b74b8f1f43a1772b6fa1bb
Rakibul Islam <72851692+Rakibul73@users.noreply.github.com>
2021-05-31 13:30:20 +0600

    learned c# file

---
+++ programing_code/C#/Test3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine()
+                .Append('=', 20)
+                .AppendLine()
+                .Append("\tYo Yo")
+                .AppendLine()
+                .Append('=', 20);
+            Console.WriteLine(builder);
+            //=====================================================
+            var filepath = @"C:\Users\Spry-Shanto\Desktop\test.txt";
+            //var f = File.ReadAllText(filepath);
+            //Console.WriteLine(f);
+            //===================================================
+            //Console.WriteLine("Extension = " + Path.GetExtension(filepath));
+            //=================================================
+            Console.WriteLine("Write a program that reads a text file and displays the number of words & " +
+                "longest word in the file.");
+            var s = File.ReadAllText(filepath);
+            string[] words = s.Split(' ');
+            var big = words.Length;
+            int index = 0;
+            Console.WriteLine(words.Length);
+            for(int i = 1; i < words.Length; i++)
+            {
+                if(words[i].Length > big)
+                {
+                    big = words[i].Length;
+                    index = i;
+                }
+            }
+            Console.WriteLine(words[index]);
+            //======================================================================
+
+
+
+        }
+    }
+}
