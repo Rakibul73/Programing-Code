5b558eaabb9412eea4e14ed0c5cf500e325fdd91
Rakibul Islam <72851692+Rakibul73@users.noreply.github.com>
2021-05-31 13:52:05 +0600

    another problem solved

---
+++ programing_code/Codeforces/1529A.cpp
@@ -0,0 +1,47 @@
+
+/***
+    Bismillahir Rahmanir Rahim
+    Read in the name of Allah, who created you.
+    Author : Md. Rakibul Islam,
+    Faculty of CSE, Patuakhali Science & Technology University, Bangladesh.
+***/
+
+// last modified: 2021/05/24 20:51:07
+
+
+
+#include <bits/stdc++.h> 
+using namespace std;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    int count = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if(a[i] != a[0]) {
+            count++;
+        }
+    }
+    cout << count << "\n";
+}
+
+int main() {
+    // Below function is A fast IO program
+    fast_cin();
+    int t;
+    cin >> t;
+    //t = 1;
+    for (int i = 1; i <= t; i++) {
+        solve();
+    }
+    
+    return 0;
+}
