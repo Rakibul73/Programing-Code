72755117e9306cb013b9a5600414a57642d8f2ff
Rakibul73 <rakibul29185@yahoo.com>
2022-10-27 17:26:48 +0600

    solved some solution again

---
+++ programing_code/Codeforces/zzzzzzzzz.cpp
@@ -0,0 +1,129 @@
+/***
+    Bismillahir Rahmanir Rahim
+    Read in the name of Allah, who created you.
+    Author : Md. Rakibul Islam,
+    Faculty of CSE, Patuakhali Science & Technology University, Bangladesh.
+***/
+
+// last modified: 2022/10/23 17:53:59
+
+
+
+#include <bits/stdc++.h> 
+using namespace std;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+int INF = 1000000005;
+long long INFF = 1000000000000000005LL;
+
+map<char, int> string_TO_map_With_Freq(string str) {
+    map<char, int> abc;
+    for (long long i = 0; i < str.size(); i++)
+    {
+        abc[str[i]]++;
+    }
+    abc.erase(' '); // map thika space key remove krsi.
+    return abc;
+}
+vector<pair<char, int>> map_to_vector(const map<char,int> &map) {
+    return vector<pair<char,int>>(map.begin(), map.end());
+}
+vector<pair<int,int>>  cin_vec_pair(int n) {
+    int q, w;
+    vector<pair<int,int>> a;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>q>>w;
+        a.push_back(make_pair(q,w));
+        
+    }
+    return a;
+}
+long long gcd_fast(long long a, long long b)
+{
+    if (b == 0)
+        return a;
+    return gcd_fast(b, a % b);
+}
+long long lcm_calculate_for_two_num (long long a, long long b) {
+    long long c = gcd_fast(a, b);
+    return (a*b)/c;
+}
+
+int* GetBigInteger_by_string_input_to_int_array(string str) 
+{ 
+    int x = str.size(), a = 0; 
+    int* arr = new int[str.size()]; 
+    while (a != x) { 
+        arr[a] = str[a] - '0'; 
+        a++; 
+    }
+    return arr; 
+}
+
+int count_Divisors(int n) 
+{ 
+    int cnt = 0; 
+    for (int i = 1; i <= sqrt(n); i++) { 
+        if (n % i == 0) { 
+            if (n / i == i) 
+                cnt++;
+            else
+                cnt = cnt + 2; 
+        } 
+    } 
+    return cnt; 
+} 
+
+void solve() {
+    int n, count = 0;
+    cin>>n;
+    string bal;
+    cin>>bal;
+    if(bal.size() == 1) {
+        cout<<"0\n";
+        return;
+    }
+    for (int i = 0; i < bal.size(); i++)
+    {
+        if(bal[i] == '1') {
+            for (int k = i+1; k < bal.size(); k++)
+            {
+                if(bal[k] == '0') {
+                    break;
+                }
+                if(bal[k] == '1') {
+                    continue;
+                }
+            }
+            
+            count++;
+            for (int j = i; j < bal.size(); j++)
+            {
+                if(bal[j] == '0') {
+                    bal[j] = '1';
+                }
+                else {
+                    bal[j] = '0';
+                }
+            }
+            
+        }
+    }
+    
+    cout<<count<<"\n";
+    return;
+}
+
+int main() {
+    // Below function is A fast IO program
+    fast_cin();
+    int t;
+    cin >> t;
+    //t = 1;
+    for (int i = 1; i <= t; i++) {
+        solve();
+    }
+    
+    return 0;
+}
