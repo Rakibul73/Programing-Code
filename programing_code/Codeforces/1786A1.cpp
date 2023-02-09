911d42ec52588bcf7abd2bc267d4209eceabb730
Rakibul73 <rakibul29185@yahoo.com>
2023-02-09 12:31:34 +0600

    solved another contest prb

---
+++ programing_code/Codeforces/1786A1.cpp
@@ -0,0 +1,126 @@
+/***
+    Bismillahir Rahmanir Rahim
+    Read in the name of Allah, who created you.
+    Author : Md. Rakibul Islam,
+    Faculty of CSE, Patuakhali Science & Technology University, Bangladesh.
+***/
+
+// last modified: 2023/02/05 18:24:26
+
+
+
+#include <bits/stdc++.h> 
+using namespace std;
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+
+int INF = 1000000005;
+long long INFF = 1000000000000000005LL;
+
+map<char, int> string_TO_map_With_Freq(string str) {
+    map<char, int> abc;
+    for (long long i = 0; i < str.size(); i++)
+    {
+        abc[str[i]]++;
+    }
+    abc.erase(' '); // map thika space key remove krsi.
+    return abc;
+}
+vector<pair<char, int>> map_to_vector(const map<char,int> &map) {
+    return vector<pair<char,int>>(map.begin(), map.end());
+}
+vector<pair<int,int>>  cin_vec_pair(int n) {
+    int q, w;
+    vector<pair<int,int>> a;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>q>>w;
+        a.push_back(make_pair(q,w));
+        
+    }
+    return a;
+}
+long long gcd_fast(long long a, long long b)
+{
+    if (b == 0)
+        return a;
+    return gcd_fast(b, a % b);
+}
+long long lcm_calculate_for_two_num (long long a, long long b) {
+    long long c = gcd_fast(a, b);
+    return (a*b)/c;
+}
+
+int* GetBigInteger_by_string_input_to_int_array(string str) 
+{ 
+    int x = str.size(), a = 0; 
+    int* arr = new int[str.size()]; 
+    while (a != x) { 
+        arr[a] = str[a] - '0'; 
+        a++; 
+    }
+    return arr; 
+}
+
+int count_Divisors(int n) 
+{ 
+    int cnt = 0; 
+    for (int i = 1; i <= sqrt(n); i++) { 
+        if (n % i == 0) { 
+            if (n / i == i) 
+                cnt++;
+            else
+                cnt = cnt + 2; 
+        } 
+    } 
+    return cnt; 
+} 
+
+void solve() {
+    long long bal;
+    cin>>bal;
+    long long sum_a = 1, sum_b = 0;
+    bal--;
+
+    bool gggg = true; // b re pala
+    for (long long i = 2; bal > 0; i = i + 2)
+    {
+        if(bal > (i + i + 1) && gggg == true) {
+            sum_b = sum_b + i + i + 1;
+            gggg = false;
+            bal = bal - (i + i + 1);
+            continue;
+        }
+        if(bal < (i+i+1) && gggg == true) {
+            sum_b = sum_b + bal;
+            gggg = false;
+            break;
+        }
+
+        if(bal > (i + i + 1) && gggg == false) {
+            sum_a = sum_a + i + i + 1;
+            gggg = true;
+            bal = bal - (i + i + 1);
+            continue;
+        }
+        if(bal < (i+i+1) && gggg == false) {
+            sum_a = sum_a + bal;
+            gggg = true;
+            break;
+        }
+    }
+    cout<<sum_a<<" "<<sum_b<<"\n";
+    return;
+}
+
+int main() {
+    // Below function is A fast IO program
+    fast_cin();
+    int t;
+    cin >> t;
+    //t = 1;
+    for (int i = 1; i <= t; i++) {
+        solve();
+    }
+    
+    return 0;
+}
