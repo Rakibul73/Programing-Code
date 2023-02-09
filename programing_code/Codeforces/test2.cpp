20bc83da1272e9c6e80f835c275f78739df8df6a
Rakibul73 <rakibul29185@yahoo.com>
2023-02-09 12:35:42 +0600

    some config file

--- programing_code/Codeforces/test2.cpp
+++ programing_code/Codeforces/test2.cpp
@@ -1,106 +1,58 @@
-/***
-    Bismillahir Rahmanir Rahim
-    Read in the name of Allah, who created you.
-    Author : Md. Rakibul Islam,
-    Faculty of CSE, Patuakhali Science & Technology University, Bangladesh.
-***/
-
-// last modified: 2022/11/22 15:46:14
-
-
-
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define pii pair<int, int>
+#define pll pair<long long int, long long int>
+#define ALL(s) (s).begin(), (s).end()
+#define rALL(s) (s).rbegin(), (s).rend()
+#define show(x) cout << #x << " : " << x << endl
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+template <class T>
+using indexed_set = tree<T, null_type, less<T>, rb_tree_tag,
+                         tree_order_statistics_node_update>; // indexed_set<ll> st;st.order_of_key(x);
+const long long mod = 1e9 + 7;
 
-int INF = 1000000005;
-long long INFF = 1000000000000000005LL;
+int dx[]={0, 0, +1, -1};
+int dy[]={+1, -1, 0, 0};
 
-map<char, int> string_TO_map_With_Freq(string str) {
-    map<char, int> abc;
-    for (long long i = 0; i < str.size(); i++)
-    {
-        abc[str[i]]++;
-    }
-    abc.erase(' '); // map thika space key remove krsi.
-    return abc;
-}
-vector<pair<char, int>> map_to_vector(const map<char,int> &map) {
-    return vector<pair<char,int>>(map.begin(), map.end());
-}
-vector<pair<int,int>>  cin_vec_pair(int n) {
-    int q, w;
-    vector<pair<int,int>> a;
-    for (int i = 0; i < n; i++)
-    {
-        cin>>q>>w;
-        a.push_back(make_pair(q,w));
-        
-    }
-    return a;
-}
-long long gcd_fast(long long a, long long b)
+void solve(int tc)
 {
-    if (b == 0)
-        return a;
-    return gcd_fast(b, a % b);
-}
-long long lcm_calculate_for_two_num (long long a, long long b) {
-    long long c = gcd_fast(a, b);
-    return (a*b)/c;
-}
+  //cout << "Case " << tc << ": ";
+  ll n;
+  cin >> n;
+  string s;
+  cin >> s;
+  set<char>x, y;
+  ll left[n], right[n];
+  for(int i=0; i<n; i++){
+    x.insert(s[i]);
+    left[i]=x.size();
+  }
+  for(int i=n-1; i>=0; i--){
+    y.insert(s[i]);
+    right[i]=y.size();
+  }
 
-int* GetBigInteger_by_string_input_to_int_array(string str) 
-{ 
-    int x = str.size(), a = 0; 
-    int* arr = new int[str.size()]; 
-    while (a != x) { 
-        arr[a] = str[a] - '0'; 
-        a++; 
-    }
-    return arr; 
-}
-
-int count_Divisors(int n) 
-{ 
-    int cnt = 0; 
-    for (int i = 1; i <= sqrt(n); i++) { 
-        if (n % i == 0) { 
-            if (n / i == i) 
-                cnt++;
-            else
-                cnt = cnt + 2; 
-        } 
-    } 
-    return cnt; 
-} 
-
-void solve() {
-    
-    for (int i = 0; i < 2*100000; i++)
-    {
-        cout<<"0 ";
-        cout<<"1 ";
-        cout<<"0 ";
-        cout<<"1 ";
-        cout<<"1 ";
-        cout<<"0 ";
-        cout<<"0 ";
-        cout<<"0 ";
-        cout<<"1 ";
-    }
-    
+  ll ans = 0;
+  for(int i=0; i<n-1; i++){
+    ll tem  = left[i]+right[i+1];
+    ans = max(ans, tem);
+  }
+  cout <<ans << endl;
 }
 
-int main() {
-    // Below function is A fast IO program
-    fast_cin();
-    int t;
-    //cin >> t;
-    t = 1;
-    for (int i = 1; i <= t; i++) {
-        solve();
-    }
-    
-    return 0;
+int main()
+{
+    int tc = 1;
+    cin >> tc;
+    for (int i = 1; i <= tc; i++)
+        solve(i);
 }
